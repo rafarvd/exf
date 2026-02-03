9fd72788fc4869f17f7cc8d359751ae23925d448
rafavd <rafarovd@gmail.com>
2026-02-03 00:00:35 -0300

    Refactor app.py for environment variables and error handling
    Refactor app.py to use environment variables for URLs, login, and API keys. Enhance error handling and add loop for multiple URL accesses.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+import random
+import os
 
 # uv pip install -U camoufox
 from browserforge.fingerprints import Screen
@@ -8,11 +10,16 @@ from playwright.async_api import Page
 from hcaptcha_challenger import AgentV, AgentConfig, CaptchaResponse
 from hcaptcha_challenger.utils import SiteKey
 
+URLS = os.getenv("URLS").split(",")
+LOGIN = os.getenv("LOGIN")
+REF = os.getenv("REF")
+API_KEY = os.getenv("GEMINI_API_KEY").split(",")
 
 async def challenge(page: Page) -> AgentV:
     """Automates the process of solving an hCaptcha challenge."""
     # [IMPORTANT] Initialize the Agent before triggering hCaptcha
-    agent_config = AgentConfig(DISABLE_BEZIER_TRAJECTORY=True)
+    agent_config = AgentConfig(
+        DISABLE_BEZIER_TRAJECTORY=True, GEMINI_API_KEY=random.choice(API_KEY))
     agent = AgentV(page=page, agent_config=agent_config)
 
     # In your real-world workflow, you may need to replace the `click_checkbox()`
@@ -28,6 +35,7 @@ async def challenge(page: Page) -> AgentV:
 async def main():
     async with AsyncCamoufox(
         headless=True,
+        # headless="virtual",
         persistent_context=True,
         user_data_dir="tmp/.cache/camoufox",
         screen=Screen(max_width=1366, max_height=768),
@@ -36,32 +44,48 @@ async def main():
     ) as browser:
         page = browser.pages[-1] if browser.pages else await browser.new_page()
 
-        await page.goto("https://ex-faucet.xyz/faucet-pepe?r=rafaro128@gmail.com", wait_until="domcontentloaded")
-        await page.wait_for_timeout(2000)
-        await page.wait_for_selector("#address")
-        if not await page.input_value("#address"):
-            await page.type("#address", "rafarovd@gmail.com", delay=50)
-        await page.wait_for_timeout(2000)
-        await page.wait_for_selector('button:has-text("Start Claim")')
-        await page.click('button:has-text("Start Claim")')
-        await page.wait_for_timeout(2000)
+        for i in range(1, 16):
+            try:
+                URL = random.choice(URLS)
+                msg = f"[{i}/15] Accessing..."
+                print(f"╔{'═' * (len(msg) + 4)}╗")
+                print(f"║  {msg}  ║")
+                print(f"╚{'═' * (len(msg) + 4)}╝")
+                await page.goto(f"{URL}?r={REF}", wait_until="domcontentloaded")
+                await page.wait_for_timeout(2000)
+                await page.wait_for_selector("#address")
+                if not await page.input_value("#address"):
+                    await page.type("#address", LOGIN, delay=50)
+                await page.wait_for_timeout(2000)
+                await page.wait_for_selector('button:has-text("Start Claim")')
+                await page.click('button:has-text("Start Claim")')
+                await page.wait_for_timeout(2000)
 
-        # --- When you encounter hCaptcha in your workflow ---
-        agent: AgentV = await challenge(page)
+                # --- When you encounter hCaptcha in your workflow ---
+                agent: AgentV = await challenge(page)
 
-        # Print the last CaptchaResponse
-        if agent.cr_list:
-            cr: CaptchaResponse = agent.cr_list[-1]
-            print(json.dumps(cr.model_dump(by_alias=True),
-                  indent=2, ensure_ascii=False))
-        
-        await page.screenshot(path="screen.png", full_page=True)
-        await page.wait_for_timeout(2000)
-        await page.wait_for_selector('input#login')
-        await page.click('input#login')
-        await page.screenshot(path="screen.png", full_page=True)
-        await page.wait_for_timeout(2000)
+                # Print the last CaptchaResponse
+                if agent.cr_list:
+                    cr: CaptchaResponse = agent.cr_list[-1]
+                    # print(json.dumps(cr.model_dump(by_alias=True),
+                    #       indent=2, ensure_ascii=False))
+
+                await page.screenshot(path="screen.png", full_page=True)
+                await page.wait_for_timeout(2000)
+                await page.wait_for_selector('input#login')
+                await page.click('input#login')
+                await page.screenshot(path="screen.png", full_page=True)
+                await page.wait_for_timeout(2000)
+                try:
+                    await page.wait_for_selector("div.alert-success", timeout=5000)
+                    sucesso = await page.inner_text("div.alert-success")
+                    print(f"\n\033[1;32mSUCCESS\033[0m | {sucesso}\n")
+                except Exception as e:
+                    print(f"\n\033[1;31mFAILURE\033[0m | No success message found.\n")
+                    continue
+            except Exception as e:
+                print(f"\n\033[1;31mERROR\033[0m | Critical error occurred.\n")
+                continue
 
 if __name__ == "__main__":
     asyncio.run(main())
-
